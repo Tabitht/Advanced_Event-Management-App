cbc878e7cb227d94841b8995d861c6ba8fd8689d
Tabitht <imafidontabitha@gmail.com>
2025-09-03 18:18:22 +0100

    installed necessary packages and dependencies and switched to typescript

---
+++ MeetHive_Backend/eslint.config.mts
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
+
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      prettierConfig,
+    ],
+    languageOptions: { globals: globals.node },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+]);
--- MeetHive_Backend/src/config/db.ts
+++ MeetHive_Backend/src/config/db.ts
@@ -1,14 +1,12 @@
 /**
- * @file db.js
- * @module src/config/db.js
+ * @file db.ts
+ * @module src/config/db.ts
  * @description Database configuration using pg (node-postgres)
  */
-import pkg from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const { Pool } = pkg;
 
 // Create a new pool instance with configuration from environment variables
 const pool = new Pool({
@@ -16,20 +14,18 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
 });
 
 // Function to connect to the database and handle connection errors
-const connectDB = async () => {
+export const connectDB = async (): Promise<void> => {
   try {
     await pool.connect();
-    console.log("Connected to PostgreSQL Suscessfully");
+    console.log("Connected to PostgreSQL successfully");
   } catch (err) {
-    console.error("Connection error:", err.message);
-    process.exit(1); 
+    console.error("Connection error:", (err as Error).message);
+    process.exit(1);
   }
 };
-
-connectDB();
 
 export default pool;
