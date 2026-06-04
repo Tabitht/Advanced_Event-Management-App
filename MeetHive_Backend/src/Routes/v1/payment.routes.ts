a2dc99c761382e49b9816c3376ee84b225ce54ed
Tabitht <imafidontabitha@gmail.com>
2026-06-04 20:55:27 +0100

    Fix: Restructured the payment and ticket models and added an order model

---
+++ MeetHive_Backend/src/Routes/v1/payment.routes.ts
@@ -0,0 +1,6 @@
+/**
+ * @module src/Routes/v1/payment.routes.ts
+ * @description Routes for payment-related endpoints.
+ */
+import { Router } from "express";
+import authenticate from "../../Middleware/authenticate.middleware.js";
